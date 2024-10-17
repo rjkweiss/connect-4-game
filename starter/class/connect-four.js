19c46346d1ff1f3e1bfca1dfe6f0887357ad40db
Joyce Kimojino-Weiss <jkimojinoweiss@gmail.com>
2024-10-17 12:22:33 -0500

    updating the code so that we can run the code on any size board, not only a 4x4 board

--- starter/class/connect-four.js
+++ starter/class/connect-four.js
@@ -7,15 +7,17 @@ class ConnectFour {
 
     this.playerTurn = "O";
 
-    this.grid = [[' ',' ',' ',' '],
-                 [' ',' ',' ',' '],
-                 [' ',' ',' ',' '],
-                 [' ',' ',' ',' ']];
+    this.grid = [[' ',' ',' ',' ',' ',' ',' '],
+                 [' ',' ',' ',' ',' ',' ',' '],
+                 [' ',' ',' ',' ',' ',' ',' '],
+                 [' ',' ',' ',' ',' ',' ',' '],
+                 [' ',' ',' ',' ',' ',' ',' '],
+                 [' ',' ',' ',' ',' ',' ',' ']];
 
-    this.cursor = new Cursor(4, 4);
+    this.cursor = new Cursor(6, 7);
 
     // Initialize a 6x7 connect-four grid
-    Screen.initialize(4, 4);
+    Screen.initialize(6, 7);
     Screen.setGridlines(true);
     this.updateGrid();
 
@@ -36,8 +38,8 @@ class ConnectFour {
   }
 
   updateGrid() {
-    for (let row = 0; row < 4; row++) {
-      for (let col = 0; col < 4; col++) {
+    for (let row = 0; row < this.grid.length; row++) {
+      for (let col = 0; col < this.grid[0].length; col++) {
         Screen.setGrid(row, col, this.grid[row][col]);
         Screen.setTextColor(row, col, 'green');
       }
@@ -66,39 +68,51 @@ class ConnectFour {
   }
 
   static checkWin(grid) {
+
     let rows = grid.length, cols = grid[0].length;
-    // Return 'X' if player X wins or 'O' if player O wins
+
+    // directions that we can move at each (row, col)
+    let directions = [[0, 1], [1, 0], [1, 1], [1, -1]];
+
+    // check if there is a winner in horizontal, vertical or diagonal directions
     for (let row = 0; row < rows; row++) {
-      // record each player's markers on each row
-      let col1 = grid[row][0], col2 = grid[row][1], col3 = grid[row][2], col4 = grid[row][3];
-      if(col1 === col2 && col2 === col3 && col3 === col4 && col1 != " ") {
-        return col1;
+      for (let col = 0; col < cols; col++) {
+        const currCell = grid[row][col];
+
+        if (currCell !== " ") {
+          for (let [dr, dc] of directions) {
+            if (ConnectFour.checkDirection(grid, row, col, dr, dc)) {
+              return currCell;
+            }
+          }
+        }
       }
-
-      let row1 = grid[0][row], row2 = grid[1][row], row3 = grid[2][row], row4 = grid[3][row];
-
-      if (row1 === row2 && row2 === row3 && row3 === row4 && row1 !== " ") {
-        return row1;
-      }
     }
-
-    // check diagonal
-    if (grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2]&& grid[2][2] === grid[3][3]&& grid[0][0] !== " ") {
-      return grid[0][0];
-    }
-
-    if (grid[0][3] === grid[1][2] && grid[1][2] === grid[2][1]&& grid[2][1] === grid[3][0]&& grid[0][3] !== " ") {
-      return grid[0][3];
-    }
-
 
     // Return 'T' if the game is a tie
     if(grid.flat().every(cell => cell !== " ")) {
       return "T";
     }
+
     // Return false if the game has not ended
     return false;
 
+  }
+
+  static checkDirection(grid, row, col, dr, dc) {
+
+    const currCell = grid[row][col];
+
+    for (let index = 0; index < 4; index++) {
+      const nextRow = row + index * dc;
+      const nextCol = col + index * dr;
+
+      // check if boundaries are valid or we found a cell with other player's mark
+      if (nextRow < 0 || nextRow >= grid.length || nextCol < 0 || nextCol >= grid[0].length || grid[nextRow][nextCol] !== currCell) {
+        return false;
+      }
+    }
+    return true;
   }
 
   static endGame(winner) {
